b4118899dda78013f0d597b65d362c6392b5436d
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-01-24 12:50:56 +0330

    Responsive Mobile Menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,26 +1,41 @@
+import { useState } from "react";
+
+import { TbShoppingBag } from "react-icons/tb";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 import NikeLogo from "../assets/nike-logo.svg?react";
 
 const ROUTES = ["Home", "About", "Services", "Pricing", "Contact"];
 const Nav = () => {
+  const [isMobileMenuShow, setIsMobileMenuShow] = useState(false);
+
   return (
     <nav className="flex flex-wrap items-center justify-between">
       <a href="#">
         <NikeLogo className="h-20 w-20" />
       </a>
+
       {/* Burger button */}
-      <button className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200">
+      <button
+        onClick={() => setIsMobileMenuShow(!isMobileMenuShow)}
+        className="rounded-lg p-2 hover:bg-gray-100 focus:ring-2 focus:ring-gray-200 lg:hidden"
+      >
         <RxHamburgerMenu size={25} />
       </button>
-      {/* Menu List */}
 
-      <div className="w-full">
-        <ul className="rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg">
-          {ROUTES.map((route, index) => {
+      {/* Menu List */}
+      <div
+        className={`${!isMobileMenuShow && "hidden"} w-full lg:block lg:w-auto`}
+      >
+        <ul className="flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 text-lg lg:flex-row lg:space-x-8 lg:border-none lg:bg-transparent">
+          {ROUTES.map((route, i) => {
             return (
               <li
-                className={`cursor-pointer rounded px-3 py-2 ${index === 0 ? "bg-blue-500 text-white" : "hover:bg-gray-100"}`}
+                className={`cursor-pointer rounded px-3 py-2 ${
+                  i === 0
+                    ? "bg-blue-500 text-white lg:bg-transparent lg:text-blue-500"
+                    : "hover:bg-gray-100"
+                }`}
                 key={route}
               >
                 {route}
@@ -28,6 +43,13 @@ const Nav = () => {
             );
           })}
         </ul>
+      </div>
+
+      {/* Cart button */}
+      <div className="fixed bottom-4 left-4 lg:static">
+        <div className="flex-center h-12 w-12 rounded-full bg-white shadow-md">
+          <TbShoppingBag />
+        </div>
       </div>
     </nav>
   );
