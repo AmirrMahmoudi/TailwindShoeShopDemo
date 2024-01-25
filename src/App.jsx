e1f4da50ce1220b38770fae97614ba2f322fa2ab
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-01-25 18:08:49 +0330

    Dark mode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import NewArrivalsSection from "./components/NewArrivalsSection";
 import ShoeDetail from "./components/ShoeDetail";
@@ -6,6 +6,7 @@ import Sidebar from "./components/Sidebar";
 import { SHOE_LIST } from "./constant";
 // import CartItem from "./components/CartItem";
 import Cart from "./components/Cart";
+import { BiMoon, BiSun } from "react-icons/bi";
 
 const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
   return {
@@ -17,6 +18,20 @@ const FAKE_CART_ITEMS = SHOE_LIST.map((shoe) => {
 
 function App() {
   const [isSidearOpen, setIsSidearOpen] = useState(false);
+
+  useEffect(() => {
+    const isDarkMode = localStorage.getItem("isDarkMode");
+    if (isDarkMode === "true") {
+      window.document.documentElement.classList.add("dark");
+    }
+  }, []);
+  const toggleDarkMode = () => {
+    window.document.documentElement.classList.toggle("dark");
+    localStorage.setItem(
+      "isDarkMode",
+      window.document.documentElement.classList.contains("dark"),
+    );
+  };
   return (
     <div className="animate-fadeIn p-10 xl:px-24">
       <Nav onClickShoppingBtn={() => setIsSidearOpen(true)} />
@@ -28,6 +43,15 @@ function App() {
       >
         <Cart cartItems={FAKE_CART_ITEMS} />
       </Sidebar>
+      <div className="fixed bottom-4 right-4">
+        <button
+          onClick={toggleDarkMode}
+          className="bg-night-50 dark:text-night-50 rounded-full px-4 py-2 text-white shadow-lg dark:bg-white"
+        >
+          <BiSun className="hidden dark:block" />
+          <BiMoon className="dark:hidden" />
+        </button>
+      </div>
     </div>
   );
 }
